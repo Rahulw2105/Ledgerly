391089e381c966424bda58b7fefbb3872a555826
Rahul Wankhede <wankhederahul116@gmail.com>
2026-03-29 00:50:34 +0530

    Setup backend project structure with Express, MongoDB config, and environment setup

---
+++ backend/src/config/Db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log("MONGO Connected");
+    }
+    catch(err) {
+        console.log("Error:" , err.message);
+    }
+}
+
+
---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+//console.log(app);
+
+app.use
+const port = process.env.PORT || 5000
+
+app.listen (port , () => {
+    console.log("Server is running");
+})
