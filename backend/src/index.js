f1cacf88344cab13ce2ec46d62444550a11d4978
Rahul Wankhede <wankhederahul116@gmail.com>
2026-03-29 14:59:59 +0530

    add cors, body parser, cookir-parser and authRoutes middlwares

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,20 @@
 require("dotenv").config();
-
 const express = require("express");
-
+const cors = require("cors")
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/Db")
+const authRoutes = require('./routes/authRoutes');
 const app = express();
 
 //console.log(app);
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
 
-app.use
+app.use('/api/auth',authRoutes);
+
 const port = process.env.PORT || 5000
-
+connectDB();
 app.listen (port , () => {
     console.log("Server is running");
 })
